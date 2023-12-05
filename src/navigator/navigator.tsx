cca436b0075bfd5d03126ea74d6257ce8b86d5f5
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2023-12-05 13:59:44 -0400

    m

--- src/navigator/navigator.tsx
+++ src/navigator/navigator.tsx
@@ -39,11 +39,16 @@ export const Navigator = () => {
       
           (status !== 'authenticated')
           ? (<>
-                  <Stack.Screen name="RegistrodatosFigmaScreen" component={ RegistrodatosFigmaScreen } />
-                  <Stack.Screen name="SeguridadFigmaScreen" component={ SeguridadFigmaScreen } />
-                  <Stack.Screen name="SendCodeFigmaScreen" component={ SendCodeFigmaScreen } />
-                  <Stack.Screen name="SendPhoneFigmaScreen" component={ SendPhoneFigmaScreen } />
+                   <Stack.Screen name="RegistrodatosFigmaScreen" component={ RegistrodatosFigmaScreen } />
                   <Stack.Screen name="WelcomeScreen" component={ WelcomeScreen } />
+                  <Stack.Screen name="SendPhoneFigmaScreen" component={ SendPhoneFigmaScreen } />
+                  <Stack.Screen name="SendCodeFigmaScreen" component={ SendCodeFigmaScreen } />
+                  <Stack.Screen name="SeguridadFigmaScreen" component={ SeguridadFigmaScreen } />
+                 
+                 
+                
+                
+                
                   <Stack.Screen name="LoginScreen" component={ LoginScreen } />
                   <Stack.Screen name="SendSmsScreen" component={ SendSmsScreen } />
                   <Stack.Screen name="RegisterScreen" component={ RegisterScreen } /> 
